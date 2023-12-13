cb9973690d0dfa344f1aac016a2acbd7473a383b
Miles Angels <milesmarieangels@gmail.com>
2023-12-12 17:06:41 -0800

    solve system and server date issue to display proper neows

--- js/app.js
+++ js/app.js
@@ -164,69 +164,138 @@ async function displayApodStart(start) {
 function resetUI() {
     const apodContent = document.getElementById('apod-content');
     const apod = document.querySelector('.card');
+    const neowsContent = docuement.getElementById('neows-content');
+    const neows = document.querySelector('.card');
 
     console.log(apodContent);
     if (apodContent.length !== 0) {
         apodContent.removeChild(apod);
+        neowsContent.removeChild(neows);
+    }
+    else {
+        return;
     }
 
 }
 // NeoWS
 async function displayNeowsData() {
+    
+    // Get the system date
     const date = getSystemDate();
-    const response = await fetch(`${globalVars.api.apiURL}${globalVars.api.categories.neows}?api_key=${globalVars.api.apiKey}`);
-    const neows = await response.json();
+
+    // Instantiate a new Date object that is in UTC Date/Time (NASA API server time is in UTC time)
+    let serverDate = new Date().toISOString().slice(0,10);
+
     let title = document.getElementById('title');
-    title.innerHTML = `Near Earth Objects for ${date}`;
+    
+    // Check if the system (local) date is equal or not equal to the NASA API's server time
+    if(date !== serverDate) {
+        title.innerHTML = `Near Earth Objects for ${date}`;
+        const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=fs6RHwXud5zkYO58zcIHVBfKA2bGE5FLloRmVSJo`);
+        const neows = await response.json();
 
-    console.log(neows.links.previous);
 
-    for (let i = 0; i < neows.near_earth_objects[date].length; i++) {
+        for (let i = 0; i < neows.near_earth_objects[date].length; i++) {
 
-        // Create Cards
-        const card = document.createElement('div');
-        card.classList = 'card border p-2 mt-5';
-
-        card.innerHTML = `
-            <div class="card-body container">
-                <div class="row">
-                    <h2 class="card-title mb-4 fw-bold">Neo Name: ${neows.near_earth_objects[date][i].name}</h2>
-                    
-                    <table class="table">
-                        <thead>
-                            <th colspan="3" class="fs-3 fw-semibold">Information</th>
-                        <thead>
-                        <tbody>
-                            <tr>
-                            <th class="fw-medium" scope="row">Absolute Magnitude</th>
-                            <td colspan="3">${neows.near_earth_objects[date][i].absolute_magnitude_h}</td>
-                            </tr>
-                            <tr>
-                            <th class="fw-medium" scope="row">Minimum Estimated Diameter</th>
-                            <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.kilometers.estimated_diameter_min} km </td>
-                            <td><em>Mi:</em> ${neows.near_earth_objects[date][i].estimated_diameter.miles.estimated_diameter_min} mi </td>
-                            </tr>
-                            <tr>
-                            <th class="fw-medium" scope="row">Maximum Estimated Diameter</th>
-                            <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.kilometers.estimated_diameter_max} km </td>
-                            <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.miles.estimated_diameter_max} mi </td>
-                            </tr>
-                            <th class="fw-medium" scope="row">Potentially Hazardous</th>
-                            <td colspan="3">${neows.near_earth_objects[date][i].is_potentially_hazardous_asteroid}</td>
-                            </tr>
-                            <th class="fw-medium" scope="row">Close Approach Date</th>
-                            <td colspan="3"><em>Close Approach Date:</em> ${neows.near_earth_objects[date][i].close_approach_data[0].close_approach_date_full}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <p class="card-text">
-                        <a href="${neows.near_earth_objects[date][i].links.self}"> More information on ${neows.near_earth_objects[date][i].name}</a>
-                    </p>
+            // Create Cards
+            const card = document.createElement('div');
+            card.classList = 'card border p-2 mt-5';
+    
+            card.innerHTML = `
+                <div class="card-body container">
+                    <div class="row">
+                        <h2 class="card-title mb-4 fw-bold">Neo Name: ${neows.near_earth_objects[date][i].name}</h2>
+                        
+                        <table class="table">
+                            <thead>
+                                <th colspan="3" class="fs-3 fw-semibold">Information</th>
+                            <thead>
+                            <tbody>
+                                <tr>
+                                <th class="fw-medium" scope="row">Absolute Magnitude</th>
+                                <td colspan="3">${neows.near_earth_objects[date][i].absolute_magnitude_h}</td>
+                                </tr>
+                                <tr>
+                                <th class="fw-medium" scope="row">Minimum Estimated Diameter</th>
+                                <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.kilometers.estimated_diameter_min} km </td>
+                                <td><em>Mi:</em> ${neows.near_earth_objects[date][i].estimated_diameter.miles.estimated_diameter_min} mi </td>
+                                </tr>
+                                <tr>
+                                <th class="fw-medium" scope="row">Maximum Estimated Diameter</th>
+                                <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.kilometers.estimated_diameter_max} km </td>
+                                <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.miles.estimated_diameter_max} mi </td>
+                                </tr>
+                                <th class="fw-medium" scope="row">Potentially Hazardous</th>
+                                <td colspan="3">${neows.near_earth_objects[date][i].is_potentially_hazardous_asteroid}</td>
+                                </tr>
+                                <th class="fw-medium" scope="row">Close Approach Date</th>
+                                <td colspan="3"><em>Close Approach Date:</em> ${neows.near_earth_objects[date][i].close_approach_data[0].close_approach_date_full}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <p class="card-text">
+                            <a href="${neows.near_earth_objects[date][i].links.self}"> More information on ${neows.near_earth_objects[date][i].name}</a>
+                        </p>
+                    </div>
                 </div>
-            </div>
-        `;
-        document.getElementById('neows-content').appendChild(card);
+            `;
+            document.getElementById('neows-content').appendChild(card);
+        }
     }
+    else {
+        title.innerHTML = `Near Earth Objects for ${serverDate}`;
+        const response = await fetch(`${globalVars.api.apiURL}${globalVars.api.categories.neows}?api_key=${globalVars.api.apiKey}`);
+        const neows = await response.json();
+
+
+        for (let i = 0; i < neows.near_earth_objects[serverDate].length; i++) {
+
+            // Create Cards
+            const card = document.createElement('div');
+            card.classList = 'card border p-2 mt-5';
+    
+            card.innerHTML = `
+                <div class="card-body container">
+                    <div class="row">
+                        <h2 class="card-title mb-4 fw-bold">Neo Name: ${neows.near_earth_objects[serverDate][i].name}</h2>
+                        
+                        <table class="table">
+                            <thead>
+                                <th colspan="3" class="fs-3 fw-semibold">Information</th>
+                            <thead>
+                            <tbody>
+                                <tr>
+                                <th class="fw-medium" scope="row">Absolute Magnitude</th>
+                                <td colspan="3">${neows.near_earth_objects[serverDate][i].absolute_magnitude_h}</td>
+                                </tr>
+                                <tr>
+                                <th class="fw-medium" scope="row">Minimum Estimated Diameter</th>
+                                <td><em>KM:</em> ${neows.near_earth_objects[serverDate][i].estimated_diameter.kilometers.estimated_diameter_min} km </td>
+                                <td><em>Mi:</em> ${neows.near_earth_objects[serverDate][i].estimated_diameter.miles.estimated_diameter_min} mi </td>
+                                </tr>
+                                <tr>
+                                <th class="fw-medium" scope="row">Maximum Estimated Diameter</th>
+                                <td><em>KM:</em> ${neows.near_earth_objects[serverDate][i].estimated_diameter.kilometers.estimated_diameter_max} km </td>
+                                <td><em>KM:</em> ${neows.near_earth_objects[serverDate][i].estimated_diameter.miles.estimated_diameter_max} mi </td>
+                                </tr>
+                                <th class="fw-medium" scope="row">Potentially Hazardous</th>
+                                <td colspan="3">${neows.near_earth_objects[serverDate][i].is_potentially_hazardous_asteroid}</td>
+                                </tr>
+                                <th class="fw-medium" scope="row">Close Approach Date</th>
+                                <td colspan="3"><em>Close Approach Date:</em> ${neows.near_earth_objects[serverDate][i].close_approach_data[0].close_approach_date_full}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <p class="card-text">
+                            <a href="${neows.near_earth_objects[serverDate][i].links.self}"> More information on ${neows.near_earth_objects[serverDate][i].name}</a>
+                        </p>
+                    </div>
+                </div>
+            `;
+            document.getElementById('neows-content').appendChild(card);
+        }
+    }
+
 
 }
 
@@ -292,6 +361,7 @@ async function neowsSearch(event) {
         </div>
     `;
     document.getElementById('neows-content').appendChild(card);
+    neowsForm.reset();
 }
 
 // Get current date from system
@@ -324,7 +394,7 @@ function getDateVal(event) {
     }
 
     // Reset Form
-    form.reset();
+    apodForm.reset();
 
     displayApodData(startDate, endDate);
 
@@ -344,7 +414,6 @@ function init() {
         case '/neows.html':
             neowsForm.addEventListener('submit', neowsSearch);
             displayNeowsData();
-            
             break;
     }
 
