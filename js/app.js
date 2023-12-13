0687e2921d451c90497332386a4ae48e84016abb
Miles Angels <milesmarieangels@gmail.com>
2023-12-12 19:07:18 -0800

    fixed resetUI function to work properly on both pages

--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ const globalVars = {
 
 }
 
-
 // displayApodData Async Function
 async function displayApodData(start, end) {
     //console.log(start)
@@ -160,162 +159,109 @@ async function displayApodStart(start) {
 
     document.getElementById('apod-content').appendChild(card);
 }
-// If apod section is not empty, then remove items
-function resetUI() {
-    const apodContent = document.getElementById('apod-content');
-    const apod = document.querySelector('.card');
-    const neowsContent = docuement.getElementById('neows-content');
-    const neows = document.querySelector('.card');
 
-    console.log(apodContent);
-    if (apodContent.length !== 0) {
-        apodContent.removeChild(apod);
-        neowsContent.removeChild(neows);
-    }
-    else {
-        return;
-    }
 
-}
+
 // NeoWS
 async function displayNeowsData() {
-    
+
     // Get the system date
     const date = getSystemDate();
 
     // Instantiate a new Date object that is in UTC Date/Time (NASA API server time is in UTC time)
-    let serverDate = new Date().toISOString().slice(0,10);
+    let serverDate = new Date().toISOString().slice(0, 10);
 
     let title = document.getElementById('title');
-    
+
     // Check if the system (local) date is equal or not equal to the NASA API's server time
-    if(date !== serverDate) {
+    if (date !== serverDate) {
         title.innerHTML = `Near Earth Objects for ${date}`;
         const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=fs6RHwXud5zkYO58zcIHVBfKA2bGE5FLloRmVSJo`);
         const neows = await response.json();
-
-
-        for (let i = 0; i < neows.near_earth_objects[date].length; i++) {
-
-            // Create Cards
-            const card = document.createElement('div');
-            card.classList = 'card border p-2 mt-5';
-    
-            card.innerHTML = `
-                <div class="card-body container">
-                    <div class="row">
-                        <h2 class="card-title mb-4 fw-bold">Neo Name: ${neows.near_earth_objects[date][i].name}</h2>
-                        
-                        <table class="table">
-                            <thead>
-                                <th colspan="3" class="fs-3 fw-semibold">Information</th>
-                            <thead>
-                            <tbody>
-                                <tr>
-                                <th class="fw-medium" scope="row">Absolute Magnitude</th>
-                                <td colspan="3">${neows.near_earth_objects[date][i].absolute_magnitude_h}</td>
-                                </tr>
-                                <tr>
-                                <th class="fw-medium" scope="row">Minimum Estimated Diameter</th>
-                                <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.kilometers.estimated_diameter_min} km </td>
-                                <td><em>Mi:</em> ${neows.near_earth_objects[date][i].estimated_diameter.miles.estimated_diameter_min} mi </td>
-                                </tr>
-                                <tr>
-                                <th class="fw-medium" scope="row">Maximum Estimated Diameter</th>
-                                <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.kilometers.estimated_diameter_max} km </td>
-                                <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.miles.estimated_diameter_max} mi </td>
-                                </tr>
-                                <th class="fw-medium" scope="row">Potentially Hazardous</th>
-                                <td colspan="3">${neows.near_earth_objects[date][i].is_potentially_hazardous_asteroid}</td>
-                                </tr>
-                                <th class="fw-medium" scope="row">Close Approach Date</th>
-                                <td colspan="3"><em>Close Approach Date:</em> ${neows.near_earth_objects[date][i].close_approach_data[0].close_approach_date_full}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <p class="card-text">
-                            <a href="${neows.near_earth_objects[date][i].links.self}"> More information on ${neows.near_earth_objects[date][i].name}</a>
-                        </p>
-                    </div>
-                </div>
-            `;
-            document.getElementById('neows-content').appendChild(card);
-        }
+        // Iterate through the items in the object
+        neowsItemLoop(neows, date);
     }
     else {
         title.innerHTML = `Near Earth Objects for ${serverDate}`;
         const response = await fetch(`${globalVars.api.apiURL}${globalVars.api.categories.neows}?api_key=${globalVars.api.apiKey}`);
         const neows = await response.json();
 
-
-        for (let i = 0; i < neows.near_earth_objects[serverDate].length; i++) {
-
-            // Create Cards
-            const card = document.createElement('div');
-            card.classList = 'card border p-2 mt-5';
-    
-            card.innerHTML = `
-                <div class="card-body container">
-                    <div class="row">
-                        <h2 class="card-title mb-4 fw-bold">Neo Name: ${neows.near_earth_objects[serverDate][i].name}</h2>
-                        
-                        <table class="table">
-                            <thead>
-                                <th colspan="3" class="fs-3 fw-semibold">Information</th>
-                            <thead>
-                            <tbody>
-                                <tr>
-                                <th class="fw-medium" scope="row">Absolute Magnitude</th>
-                                <td colspan="3">${neows.near_earth_objects[serverDate][i].absolute_magnitude_h}</td>
-                                </tr>
-                                <tr>
-                                <th class="fw-medium" scope="row">Minimum Estimated Diameter</th>
-                                <td><em>KM:</em> ${neows.near_earth_objects[serverDate][i].estimated_diameter.kilometers.estimated_diameter_min} km </td>
-                                <td><em>Mi:</em> ${neows.near_earth_objects[serverDate][i].estimated_diameter.miles.estimated_diameter_min} mi </td>
-                                </tr>
-                                <tr>
-                                <th class="fw-medium" scope="row">Maximum Estimated Diameter</th>
-                                <td><em>KM:</em> ${neows.near_earth_objects[serverDate][i].estimated_diameter.kilometers.estimated_diameter_max} km </td>
-                                <td><em>KM:</em> ${neows.near_earth_objects[serverDate][i].estimated_diameter.miles.estimated_diameter_max} mi </td>
-                                </tr>
-                                <th class="fw-medium" scope="row">Potentially Hazardous</th>
-                                <td colspan="3">${neows.near_earth_objects[serverDate][i].is_potentially_hazardous_asteroid}</td>
-                                </tr>
-                                <th class="fw-medium" scope="row">Close Approach Date</th>
-                                <td colspan="3"><em>Close Approach Date:</em> ${neows.near_earth_objects[serverDate][i].close_approach_data[0].close_approach_date_full}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <p class="card-text">
-                            <a href="${neows.near_earth_objects[serverDate][i].links.self}"> More information on ${neows.near_earth_objects[serverDate][i].name}</a>
-                        </p>
-                    </div>
-                </div>
-            `;
-            document.getElementById('neows-content').appendChild(card);
-        }
+        // Iterate through the items in the object
+        neowsItemLoop(neows, serverDate);
     }
 
+}
 
+// Iterate through Neows Object
+function neowsItemLoop(neows, date) {
+
+    for (let i = 0; i < neows.near_earth_objects[date].length; i++) {
+
+        // Create Cards
+        const card = document.createElement('div');
+        card.classList = 'card border p-2 mt-5';
+
+        card.innerHTML = `
+            <div class="card-body container">
+                <div class="row">
+                    <h2 class="card-title mb-4 fw-bold">Neo Name: ${neows.near_earth_objects[date][i].name}</h2>
+                    <p>Neo Reference ID: ${neows.near_earth_objects[date][i].id}</p>
+                    <table class="table">
+                        <thead>
+                            <th colspan="3" class="fs-3 fw-semibold">Information</th>
+                        <thead>
+                        <tbody>
+                            <tr>
+                            <th class="fw-medium" scope="row">Absolute Magnitude</th>
+                            <td colspan="3">${neows.near_earth_objects[date][i].absolute_magnitude_h}</td>
+                            </tr>
+                            <tr>
+                            <th class="fw-medium" scope="row">Minimum Estimated Diameter</th>
+                            <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.kilometers.estimated_diameter_min} km </td>
+                            <td><em>Mi:</em> ${neows.near_earth_objects[date][i].estimated_diameter.miles.estimated_diameter_min} mi </td>
+                            </tr>
+                            <tr>
+                            <th class="fw-medium" scope="row">Maximum Estimated Diameter</th>
+                            <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.kilometers.estimated_diameter_max} km </td>
+                            <td><em>KM:</em> ${neows.near_earth_objects[date][i].estimated_diameter.miles.estimated_diameter_max} mi </td>
+                            </tr>
+                            <th class="fw-medium" scope="row">Potentially Hazardous</th>
+                            <td colspan="3">${neows.near_earth_objects[date][i].is_potentially_hazardous_asteroid}</td>
+                            </tr>
+                            <th class="fw-medium" scope="row">Close Approach Date</th>
+                            <td colspan="3"><em>Close Approach Date:</em> ${neows.near_earth_objects[date][i].close_approach_data[0].close_approach_date_full}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <p class="card-text">
+                        <a href="${neows.near_earth_objects[date][i].links.self}"> More information on ${neows.near_earth_objects[date][i].name}</a>
+                    </p>
+                </div>
+            </div>
+        `;
+        document.getElementById('neows-content').appendChild(card);
+    }
 }
 
 async function neowsSearch(event) {
     event.preventDefault();
+    resetUI();
     const searchItem = document.getElementById('search')
-    
+
     let item = '';
-    const regex = new RegExp(/^\d{7}?$[^\s]*/);
-    //console.log(regex.test(searchItem.value));
+    
     // Evaluate search term
     // - Search term cannot be empty string and needs to be a 7-digit number
-    if(regex.test(searchItem.value)) {
+    const regex = new RegExp(/^\d{7}?$[^\s]*/);
+
+    if (regex.test(searchItem.value)) {
         item = searchItem.value;
     }
     else {
         alert("Please enter a 7-digit number with no spaces!")
     }
     // Test Values: 3542519 2416801 3363908
-    const response = await fetch(`https://api.nasa.gov/neo/rest/v1/neo/${item}?api_key=${globalVars.api.apiKey}`);
+    const response = await fetch(`${globalVars.api.apiURL}/neo/rest/v1/neo/${item}?api_key=${globalVars.api.apiKey}`);
     const data = await response.json();
     console.log(data)
     // Create Cards
@@ -326,7 +272,7 @@ async function neowsSearch(event) {
         <div class="card-body container">
             <div class="row">
                 <h2 class="card-title mb-4 fw-bold">Neo Name: ${data.name}</h2>
-                
+                <p>Neo Reference ID: ${data.id}</p>
                 <table class="table">
                     <thead>
                         <th colspan="3" class="fs-3 fw-semibold">Information</th>
@@ -370,7 +316,7 @@ function getSystemDate() {
     const date = new Date()
     const offset = date.getTimezoneOffset();
     const localDate = new Date(date.getTime() - offset * 60000);
-    
+
     return localDate.toISOString().slice(0, 10);
 }
 
@@ -401,6 +347,21 @@ function getDateVal(event) {
 
 }
 
+// If apod content or neows content section has items, remove items
+function resetUI() {
+    const apodContent = document.getElementById('apod-content');
+
+    const neowsContent = document.getElementById('neows-content');
+
+    if (globalVars.currentPage === '/' || globalVars.currentPage === '/index.html') {
+        [...apodContent.childNodes].forEach(el => el.remove()); // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    }
+    else {
+        [...neowsContent.childNodes].forEach(el => el.remove()); // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    }
+
+}
+
 const apodForm = document.getElementById('apod-date-form');
 const neowsForm = document.getElementById('neows-date-form');
 
@@ -421,5 +382,5 @@ function init() {
 
 document.addEventListener('DOMContentLoaded', init);
 
-
-
+// create
+resetUI();
